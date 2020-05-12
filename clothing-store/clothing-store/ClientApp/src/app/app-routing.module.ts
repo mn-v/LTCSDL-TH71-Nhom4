5a06b37634d439f49282c28b8cf0bbcdbb018a42
Dios Vo <vtmn1212@gmail.com>
2020-05-12 07:48:12 +0700

    update
    update file .json

--- clothing-store/clothing-store/ClientApp/src/app/app-routing.module.ts
+++ clothing-store/clothing-store/ClientApp/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { SaleComponent } from './pages/sale/sale.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 const routes: Routes = [
     { path: '', component: HomePageComponent},
     { path: 'shop', component: ShopComponent},
-    { path: 'sale', component: SaleComponent}
-
+    { path: 'sale', component: SaleComponent},
+    { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
