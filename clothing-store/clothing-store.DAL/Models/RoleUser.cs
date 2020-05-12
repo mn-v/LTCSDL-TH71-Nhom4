585a357d0c5d66a247e95368f74f22e7cfe6bb27
= <=>
2020-05-12 21:02:39 +0700

    Create clothing-store.DAL and add models

---
+++ clothing-store/clothing-store.DAL/Models/RoleUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace clothing_store.DAL.Models
+{
+    public partial class RoleUser
+    {
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; }
+
+        public virtual Role User { get; set; }
+        public virtual Users UserNavigation { get; set; }
+    }
+}
