deb6c0e7e5877016f9d0eca91cb0cbbe3b5a6728
OrionTK <Thanhkieu24>
2020-05-19 23:43:50 +0700

    Add Product and Search Product

---
+++ clothing-store/clothing-store.DAL/ProductsRep.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using clothing_store.Common.DAL;
+
+
+namespace clothing_store.DAL
+{
+    using clothing_store.Common.Rsp;
+    using clothing_store.DAL.Models;
+    using System.Linq;
+    using System.Net.WebSockets;
+
+    public class ProductsRep : GenericRep<OnlineStoreContext, Products>
+    {
+        #region -- Override --
+        public override Products Read(int id)
+        {
+            var res = All.FirstOrDefault(p => p.ProductId == id);
+            return res;
+        }
+
+        public int Remove(int id)
+        {
+            var m = base.All.First(p => p.ProductId == id);
+            m = base.Delete(m);
+            return m.ProductId;
+        }
+        #endregion
+
+        #region -- Methods --
+
+        public SingleRsp CreateProduct(Products pro)
+        {
+            var res = new SingleRsp();
+            using (var context = new OnlineStoreContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Products.Add(pro);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        res.SetError(ex.StackTrace);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public SingleRsp UpdateProduct(Products pro)
+        {
+            var res = new SingleRsp();
+            using (var context = new OnlineStoreContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Products.Update(pro);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        res.SetError(ex.StackTrace);
+                    }
+                }
+            }
+            return res;
+        }
+
+        #endregion
+    }
+}
