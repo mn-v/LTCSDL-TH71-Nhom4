96555b66e6b5519564bbab7f881b5c4a64a3cb9b
Dios Vo <vtmn1212@gmail.com>
2020-05-29 14:29:15 +0700

    delete
    useless function

--- clothing-store/clothing-store/ClientApp/src/app/pages/men/men.component.ts
+++ clothing-store/clothing-store/ClientApp/src/app/pages/men/men.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-men',
   templateUrl: './men.component.html',
-  styleUrls: ['./men.component.scss']
 })
 export class MenComponent implements OnInit {
 
--- clothing-store/clothing-store/ClientApp/src/app/pages/sale/sale.component.ts
+++ clothing-store/clothing-store/ClientApp/src/app/pages/sale/sale.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sale',
   templateUrl: './sale.component.html',
-  styleUrls: ['./sale.component.scss']
 })
 export class SaleComponent implements OnInit {
 
