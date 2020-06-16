45c78e4a4763cab8cf2610ed106cf63eb86fd2d1
= <=>
2020-06-16 23:11:00 +0700

    phan trang san pham theo loai san pham

--- clothing-store/clothing-store.DAL/OnlineStoreContext.cs
+++ clothing-store/clothing-store.DAL/OnlineStoreContext.cs
@@ -27,13 +27,13 @@ namespace clothing_store.DAL.Models
         public virtual DbSet<Size> Size { get; set; }
         public virtual DbSet<Transactions> Transactions { get; set; }
         public virtual DbSet<Users> Users { get; set; }
-        public object Contact { get; internal set; }
+        public virtual DbSet<Contact> Contact { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=.\\MNV;Initial Catalog=OnlineStore;Persist Security Info=True;User ID=sa;Password=diosvo0321;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=OnlineStore;Persist Security Info=True;User ID=sa;Password=123;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
             }
         }
 
--- clothing-store/clothing-store/ClientApp/src/app/pages/women/women.component.ts
+++ clothing-store/clothing-store/ClientApp/src/app/pages/women/women.component.ts
@@ -6,9 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './women.component.html',
 })
 export class WomenComponent implements OnInit {
-  public res:any;
-  public lstCategoryName : [];
+  public res: any;
+  public lstCategoryName: [];
   public lstProduct: [];
+  flag:string ="1";
+  categoryName = "";
 
   products: any = {
     data: [],
@@ -20,40 +22,45 @@ export class WomenComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    @Inject('BASE_URL') baseUrl: string) { 
-      
-    }
+    @Inject('BASE_URL') baseUrl: string) {
+
+  }
 
   ngOnInit() {
     this.searchProductByGender(1);
   }
 
-  getProduct(cPage, name){
+  //lay danh sach san pham theo ten cua loai san pham chon o phan loai
+  getProduct(cPage, name) {
     let x = {
       page: cPage,
       size: 3,
       keyword: "",
       categoryName: name,
       gender: true
-    } 
+    }
     this.http.post('https://localhost:44320/' + 'api/Products/get-product-by-categoryName-linq', x).subscribe(result => {
+      this.flag="1";
+      this.categoryName = name;  
       this.products = result;
       this.products = this.products.data;
       console.log(this.products);
     }, error => console.error(error));
   }
 
-  getCategoryName(cGender){
-    var x ={
+  //lay danh sach ten loai san pham de hien thi len phan loai
+  getCategoryName(cGender) {
+    var x = {
       gender: cGender
-    }     
+    }
     this.http.post('https://localhost:44320/' + 'api/Categories/get-categoryname-by-gender-linq', x)
-    .subscribe(result => {
-      this.res = result;
-      this.lstCategoryName = this.res.data;
-    }, error => console.error(error));
+      .subscribe(result => {
+        this.res = result;
+        this.lstCategoryName = this.res.data;
+      }, error => console.error(error));
   }
-  
+
+  //danh sach tat ca mat hang cua nu, hien len khi load sang trang women
   searchProductByGender(cPage) {
     let x = {
       page: cPage,
@@ -62,49 +69,98 @@ export class WomenComponent implements OnInit {
       gender: true
     }
     this.http.post("https://localhost:44320/api/Products/search-product-by-gender", x).subscribe(result => {
+      this.flag="2";  
       this.products = result;
       this.products = this.products.data;
       console.log(this.products);
     }, error => console.error(error));
   }
 
+  //chia lam 2 truong hop:
+  // flag = 1 : phan trang theo phan loai san pham
+  // flag = 2 : phan trang theo san pham nu
   searchNext() {
-    if (this.products.page < this.products.totalPages) {
-      let nextPage = this.products.page + 1;
-      let x = {
-        page: nextPage,
-        size: 3,
-        keyword: "",
-        gender: true
+    if (this.flag == "1") {
+      if (this.products.page < this.products.totalPages) {
+        let nextPage = this.products.page + 1;
+        let x = {
+          page: nextPage,
+          size: 3,
+          keyword: "",
+          categoryName: this.categoryName,
+          gender: true
+        }
+        this.http.post('https://localhost:44320/' + 'api/Products/get-product-by-categoryName-linq', x).subscribe(result => {
+          this.products = result;
+          this.products = this.products.data;
+          console.log(this.products);
+        }, error => console.error(error));
       }
-      this.http.post("https://localhost:44320/api/Products/search-product-by-gender", x).subscribe(result => {
-        this.products = result;
-        this.products = this.products.data;
-        console.log(this.products);
-      }, error => console.error(error));
+      else {
+        alert("Bạn đang ở trang cuối cùng!");
+      }
     }
     else {
-      alert("Bạn đang ở trang cuối cùng!");
+      if (this.products.page < this.products.totalPages) {
+        let nextPage = this.products.page + 1;
+        let x = {
+          page: nextPage,
+          size: 3,
+          keyword: "",
+          gender: true
+        }
+        this.http.post("https://localhost:44320/api/Products/search-product-by-gender", x).subscribe(result => {
+          this.products = result;
+          this.products = this.products.data;
+          console.log(this.products);
+        }, error => console.error(error));
+      }
+      else {
+        alert("Bạn đang ở trang cuối cùng!");
+      }
     }
   }
 
+  //tuong tu searchNext()
   searchPrevious() {
-    if (this.products.page > 1) {
-      let previous = this.products.page - 1;
-      let x = {
-        page: previous,
-        size: 3,
-        keyword: "",
-        gender: true
+    if(this.flag == "1"){
+      if (this.products.page > 1) {
+        let previous = this.products.page - 1;
+        let x = {
+          page: previous,
+          size: 3,
+          keyword: "",
+          categoryName: this.categoryName,
+          gender: true
+        }
+        this.http.post('https://localhost:44320/' + 'api/Products/get-product-by-categoryName-linq', x).subscribe(result => {
+          this.products = result;
+          this.products = this.products.data;
+          console.log(this.products);
+        }, error => console.error(error));
       }
-      this.http.post("https://localhost:44320/api/Products/search-product-by-gender", x).subscribe(result => {
-        this.products = result;
-        this.products = this.products.data;
-        console.log(this.products);
-      }, error => console.error(error));
+      else {
+        alert("Bạn đang ở trang đầu tiên!");
+      }
     }
-    else {
-      alert("Bạn đang ở trang đầu tiên!");
+    else{
+      if (this.products.page > 1) {
+        let previous = this.products.page - 1;
+        let x = {
+          page: previous,
+          size: 3,
+          keyword: "",
+          gender: true
+        }
+        this.http.post("https://localhost:44320/api/Products/search-product-by-gender", x).subscribe(result => {
+          this.products = result;
+          this.products = this.products.data;
+          console.log(this.products);
+        }, error => console.error(error));
+      }
+      else {
+        alert("Bạn đang ở trang đầu tiên!");
+      }
     }
   }
 }
