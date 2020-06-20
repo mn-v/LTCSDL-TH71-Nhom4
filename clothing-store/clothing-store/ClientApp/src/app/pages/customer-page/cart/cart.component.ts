86e71fa5b839c9b30a38ec6e84b4f71f206ba4c7
OrionTK <Thanhkieu24>
2020-06-20 13:15:53 +0700

    API Order Chưa xong Page Thanh Toan Chua Luu Duoc OrderDetails

--- clothing-store/clothing-store/ClientApp/src/app/pages/customer-page/cart/cart.component.ts
+++ clothing-store/clothing-store/ClientApp/src/app/pages/customer-page/cart/cart.component.ts
@@ -12,8 +12,11 @@ export class CartComponent implements OnInit {
   cart: any = {
     data: []
   };
-  totalMoney:number= 0;
-  SumPro:any;
+  user: any = {
+    data: []
+  }
+  totalMoney: number = 0;
+  SumPro: any;
   constructor(private activateRoute: ActivatedRoute, private http: HttpClient,
     @Inject('BASE_URL') baseUrl: string, private cookieService: CookieService) {
 
@@ -22,14 +25,14 @@ export class CartComponent implements OnInit {
   ngOnInit() {
     this.userId = parseInt(this.cookieService.get("userId"));
     this.getCart(this.userId);
-    
+
   }
 
   getCart(id) {
     this.http.get("https://localhost:44320/api/Carts/get-cart/" + id, id).subscribe(result => {
       this.cart = result;
       this.cart = this.cart.data;
-      this.SumPro = "Tổng tiền (" + this.cart.length  + " sản phẩm): "; 
+      this.SumPro = "Tổng tiền (" + this.cart.length + " sản phẩm): ";
       this.cart.forEach(element => {
         this.totalMoney += element.total;
       });
@@ -47,7 +50,7 @@ export class CartComponent implements OnInit {
         if (res.success) {
           alert("Bạn đã xóa một sản phẩm ra khỏi giỏ!")
           this.getCart(this.userId);
-          
+
         }
         console.log(x);
       }, error => {
@@ -60,11 +63,70 @@ export class CartComponent implements OnInit {
   }
 
   ThanhToan() {
+
+
+    //Lấy thông tin user
+    var a = {
+      id: this.userId,
+      Keyword: ""
+    };
+
+    this.http.post("https://localhost:44320/api/Users/get-by-id", a).subscribe(result => {
+      this.user = result;
+      this.user = this.user.data;
+    }, error => {
+      console.error(error);
+      alert(error);
+    }
+    );
+
+    //thêm vào bảng order
+    let b = {
+      "orderId": 0,
+      "userId": this.userId,
+      "orderDate": "",
+      "shipName": "ABC",
+      "shipAddress": this.user.address,
+      "shipPhoneNumber": this.user.phoneNumber,
+      "status": false
+    };
+    this.http.post("https://localhost:44320/api/Order/create-order", a).subscribe(result => {
+      var res: any = result;
+      if (res.success)
+        alert("Bạn có một hóa đơn!")
+    }, error => {
+      console.error(error);
+    }
+    );
+
+    //thêm vào bảng orderDetails
+    this.cart.forEach(element => {
+      let c = {
+        "orderId": 0,
+        "productId": element.productId,
+        "size": element.size,
+        "unitPrice": element.unitPrice,
+        "quantity": element.quantity,
+        "discount": element.discountPercent
+      };
+      this.http.post("https://localhost:44320/api/Order/create-order-detail", c).subscribe(result => {
+        var res: any = result;
+        if (res.success)
+          alert("Bạn có một hóa đơn!")
+      }, error => {
+        console.error(error);
+        alert(error);
+      }
+      );
+    });
+    
+
+    //xóa giỏ
     this.cart.forEach(element => {
       var x = element;
       this.http.post("https://localhost:44320/api/Carts/delete-product-cart", x).subscribe(result => {
         var res: any = result;
-        
+
         console.log(x);
       }, error => {
         console.error(error);
@@ -73,8 +135,7 @@ export class CartComponent implements OnInit {
       );
     });
     alert("Bạn đã đặt hàng thành công!")
-   
-      
+
   }
 
 }
